bdb119bcd078059cc1dd6a53d8c27d67825589cf
jegtnes <alex.jegtnes@cxpartners.co.uk>
2015-07-07 21:38:20 +0100

    Added responsive images task

---
+++ example3-responsive-images/Gruntfile.js
@@ -0,0 +1,67 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    sass: {
+      dist: {
+        files: {
+          'dist/css/app.css': 'scss/app.scss'
+        }
+      }
+    },
+
+    copy: {
+      main: {
+        files: [
+          {
+            expand: true,
+            src: 'img/**/*.jpg',
+            dest: 'dist/'
+          }
+        ]
+      }
+    },
+
+    responsive_images: {
+      myTask: {
+        options: {
+          sizes: [{
+            name: 'small',
+            width: 320
+          },{
+            name: 'medium',
+            width: 640
+          },{
+            name: "large",
+            width: 1024
+          }]
+        },
+        files: [{
+          expand: true,
+          src: ['**/*.jpg'],
+          cwd: 'img/',
+          dest: 'dist/img/'
+        }]
+      }
+    },
+
+    responsive_images_extender: {
+      target: {
+        options: {},
+        files: [{
+          expand: true,
+          src: ['**/*.html'],
+          cwd: 'html/',
+          dest: 'dist/html'
+        }]
+      }
+    }
+  });
+
+  grunt.task.registerTask('images', ['copy', 'responsive_images_extender', 'responsive_images'])
+
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-responsive-images-extender');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+};
