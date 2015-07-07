19305faa82275da59e6b99208ee0c0df486295c4
jegtnes <alex.jegtnes@cxpartners.co.uk>
2015-07-07 20:02:33 +0100

    getting jsy with it

--- example2-browsersync/Gulpfile.js
+++ example2-browsersync/Gulpfile.js
@@ -1,7 +1,15 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concat = require('gulp-concat');
 var autoprefixer = require('gulp-autoprefixer');
 var browserSync = require('browser-sync').create();
+
+gulp.task('js', function() {
+  return gulp.src(['js/vendor/jquery.js', 'js/vendor/jquery.cookie.js', 'js/vendor/modernizr.js', 'js/vendor/placeholder.js', 'js/vendor/foundation.js', 'js/vendor/**/*.js', 'js/**/*.js'])
+    .pipe(concat('app.js'))
+    .pipe(gulp.dest('./dist/js/'))
+})
+
 
 gulp.task('sass', function () {
   gulp.src('./scss/**.*scss')
@@ -19,6 +27,8 @@ gulp.task('serve', ['sass'], function() {
   });
 
   gulp.watch("scss/**/*.scss", ['sass']);
-  gulp.watch("app/*.html")
+  gulp.watch("dist/js/**.*.js", ['js'])
+    .on('change', browserSync.reload)
+  gulp.watch("./*.html")
     .on('change', browserSync.reload);
 });
--- example2-browsersync/js/app.js
+++ example2-browsersync/js/app.js
@@ -1,5 +1,3 @@
-console.log('what!');
-alert('omg hi');
 $(document).on('ready', function() {
   $(document).foundation();
 });
